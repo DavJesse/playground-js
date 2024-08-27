5cf2e527acd102117174f22b8060594487eaf6db
davodhiambo <jesseomolo@yahoo.com>
2024-08-27 14:57:19 +0300

    files: add more-or-les.js.

---
+++ more-or-less.js
@@ -0,0 +1,11 @@
+// Declare functions
+const more = (n) => n+1;
+const less = (n) => n-1;
+const add = (m, n) => m+n;
+const sub = (m, n) => m-n;
+
+// // Print results for testing
+// console.log(more(23))
+// console.log(less(25))
+// console.log(add(14, 10))
+// console.log(sub(34, 10))
