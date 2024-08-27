3de32f7127ba42ccdb7543c4ee274bc3883ba641
davodhiambo <jesseomolo@yahoo.com>
2024-08-27 15:51:08 +0300

    files: add returns.js

---
+++ returns.js
@@ -0,0 +1,6 @@
+const id = (a) => a; // Returns the argument taken
+const getLength = (b) => b.length; // Returns length of array or string
+
+// //Print to console
+// console.log(id('str'))
+// console.log(getLength(['string', 'length']))
