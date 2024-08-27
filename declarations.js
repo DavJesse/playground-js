312cd2b3d6f2f6fb51bb3781c229f540803664e7
davodhiambo <jesseomolo@yahoo.com>
2024-08-27 11:31:21 +0300

    deep freeze keys in objects.

--- declarations.js
+++ declarations.js
@@ -1,28 +1,21 @@
 const escapeStr = '`\\/"\'';
-const arr = [4, 2]
+const arr = object.freeze([4, 2]);
 
-Object.freeze(arr)
-
-const obj = {
+const obj = object.freeze({
     str: 'a string value',
     num: 123,
     bool: true,
     undef: undefined
-}
+});
 
-Object.freeze(obj)
-
-const nested = {
-    arr: [4, undefined, 2],
-    obj: {
+const nested = object.freeze({
+    arr: object.freeze([4, undefined, 2]),
+    obj: object.freeze({
         str: 'a string',
         num: 456,
         bool: false
-    }
-}
-
-Object.freeze(nested.arr)
-Object.freeze(nested.obj)
+    })
+});
 
 // console.log(escapeStr);
 // console.log(arr);
