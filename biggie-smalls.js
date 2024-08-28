d3dce6825091716597daabb2cbd3338f08fe4c05
davodhiambo <jesseomolo@yahoo.com>
2024-08-28 15:09:35 +0300

    fix: fix biggie and smalls variables to biggie-smalls.js

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,13 +1,3 @@
-const smalls = Number.MIN_VALUE // Set to minimum value in JS
-const biggie = Number.MAX_VALUE // Set to maximum value in JS
-
-// console.log(biggie)
-// console.log(smalls)
-// console.assert(smalls === Number.MIN_VALUE, 'Test Case 1 Failed');
-// console.assert(biggie === Number.MAX_VALUE, 'Test Case 2 Failed');
-// console.assert(smalls > 0, 'Test Case 3 Failed');
-// console.assert(isFinite(biggie), 'Test Case 4 Failed');
-// console.assert(biggie - smalls === Number.MAX_VALUE, 'Test Case 5 Failed');
-// console.assert(smalls !== Infinity, 'Test Case 6 Failed');
-// console.assert(biggie !== -Infinity, 'Test Case 7 Failed');
+const smalls = Number.MIN_VALUE; // Set to minimum value in JS
+const biggie = Number.MAX_VALUE; // Set to maximum value in JS
 
