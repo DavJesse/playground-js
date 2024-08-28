9fbf52310396c3608d8b0b711a2f0c46935b9316
davodhiambo <jesseomolo@yahoo.com>
2024-08-28 17:03:30 +0300

    feat: add max and min functions to min-max.js

---
+++ min-max.js
@@ -0,0 +1,10 @@
+const max = (num1, num2) => Math.max(num1, num2);
+const min = (num1, num2) => Math.min(num1, num2);
+
+// Test functions
+// console.log(max(10, 20));
+// console.log(max(-10, -20));
+// console.log(max(-10, 20));
+// console.log(min(10, 20));
+// console.log(min(-10, -20));
+// console.log(min(-10, 20));
