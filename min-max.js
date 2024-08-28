3f373a701693c84beedcd0b78ac473e254e50864
davodhiambo <jesseomolo@yahoo.com>
2024-08-28 17:09:18 +0300

    fix: rewrite max and min functions without math.max and math.min respectively in min-max.js

--- min-max.js
+++ min-max.js
@@ -1,10 +1,16 @@
-const max = (num1, num2) => Math.max(num1, num2);
-const min = (num1, num2) => Math.min(num1, num2);
+const max = (num1, num2) => {
+    const result = num1>num2 ? num1 : num2
+    return result
+};
+const min = (num1, num2) => {
+    const result = num1<num2 ? num1 : num2
+    return result
+};
 
 // Test functions
-// console.log(max(10, 20));
-// console.log(max(-10, -20));
-// console.log(max(-10, 20));
-// console.log(min(10, 20));
-// console.log(min(-10, -20));
-// console.log(min(-10, 20));
+console.log(max(10, 20));
+console.log(max(-10, -20));
+console.log(max(-10, 20));
+console.log(min(10, 20));
+console.log(min(-10, -20));
+console.log(min(-10, 20));
