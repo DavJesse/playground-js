4ed37777aa9092bad95c832fd8cb42ecc0a2abc1
davodhiambo <jesseomolo@yahoo.com>
2024-08-28 11:25:10 +0300

    feat: add dogYears function to dog-years.js

---
+++ dog-years.js
@@ -0,0 +1,30 @@
+function dogYears(planet, age) {
+    // Lowecase 'planet' for universality
+    planet = planet.toLowerCase()
+
+    // Define planets object with all planets
+    const planets = {
+        earth: 1.0,
+        mercury: 0.2408467,
+        venus: 0.61519726,
+        mars: 1.8808158,
+        jupiter: 11.862615,
+        sartun: 29.447498,
+        uranus: 84.016846,
+        neptune: 164.79132,
+    }
+
+    if (!(planet in planets)) {
+        return `Error: Planet "${planet} not included in scope"`
+    }
+
+    // Convert to dog years
+    planetTime = (planets[planet] * 31557600);
+    dogAge = age/planetTime;
+    dogAge = (dogAge * 7).toFixed(2);
+
+    return dogAge
+}
+
+// console.log(dogYears('geaRth', 1000000000))
+// console.log(dogYears('jupiter', 1000000000))
