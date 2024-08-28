50b3db36f2be3d985bc27d6068e37e1f167e5b58
davodhiambo <jesseomolo@yahoo.com>
2024-08-28 16:48:44 +0300

    feat: add abs function to abs.js

--- abs.js
+++ abs.js
@@ -1,6 +1,12 @@
 // Returns true when num is positive
-const isPositive = (num) => num > 0; 
+const isPositive = (num) => num > 0;
 
-// console.log(isPositive(5))
-// console.log(isPositive(0))
-// console.log(isPositive(-1))
+function abs(num) {
+    // Convert to positive if num is negative
+    let result = num < 0 ? num*-1: num;
+    return result
+}
+
+console.log(abs(5))
+console.log(abs(0))
+console.log(abs(-1))
