c8c4543d952a0ddd529f11e5c3ba0b318b2f1c7a
davodhiambo <jesseomolo@yahoo.com>
2024-08-28 15:14:55 +0300

    fix: fix bounds of biggie and smalls variables to infinity and -infinity respectively in biggie-smalls.js

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,3 +1,3 @@
-const smalls = Number.MIN_VALUE; // Set to minimum value in JS
-const biggie = Number.MAX_VALUE; // Set to maximum value in JS
+const smalls = Infinity*-1; // Set to minimum value in JS
+const biggie = infinity; // Set to maximum value in JS
 
