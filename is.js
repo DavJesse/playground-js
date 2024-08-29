d2ac906d2f8676689e113732614fc84c0ece223b
davodhiambo <jesseomolo@yahoo.com>
2024-08-29 09:00:09 +0300

    fix: make adjustmento nan and obj functions from is.

--- is.js
+++ is.js
@@ -1,24 +1,24 @@
 //const is = {}
     is.num = (value) => typeof value === 'number' ? true : false,
-    is.nan = (value) => typeof value !== 'number' ? true : false,
+    is.nan = (value) => Number.isNaN(value),
     is.str = (value) => typeof value === 'string' ? true : false,
     is.bool = (value) => typeof value === 'boolean' ? true : false,
     is.undef= (value) => typeof value === 'undefined' ? true : false,
     is.def = (value) => typeof value !== 'undefined' ? true : false,
     is.arr = (value) => typeof value === 'array' ? true : false,
-    is.obj = (value) => typeof value === 'object' && value !== null ? true : false,
+    is.obj = (value) => typeof value === 'object' && value !== null,
     is.fun = (value) => typeof value === 'function' ? true : false,
     is.truthy = (value) => !!value,
     is.falsy = (value) => !value
  
-console.log(is.num(5))
-console.log(is.nan('k'))
-console.log(is.str('t'))
-console.log(is.bool(5))
-console.log(is.undef(5))
-console.log(is.def(5))
-console.log(is.arr(5))
-console.log(is.obj(5))
-console.log(is.fun(5))
-console.log(is.truthy(5))
-console.log(is.falsy(5))
+// console.log(is.num(5))
+// console.log(is.nan('k'))
+// console.log(is.str('t'))
+// console.log(is.bool(5))
+// console.log(is.undef(5))
+// console.log(is.def(5))
+// console.log(is.arr(5))
+// console.log(is.obj(5))
+// console.log(is.fun(5))
+// console.log(is.truthy(5))
+// console.log(is.falsy(5))
