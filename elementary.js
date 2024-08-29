84900b977292b8d0e6e9b2cfa57ccffbdc788276
davodhiambo <jesseomolo@yahoo.com>
2024-08-29 13:45:11 +0300

    fix: adjust multiplication function to elementary.js.

--- elementary.js
+++ elementary.js
@@ -1,11 +1,21 @@
 // Iteratively add a to itself to multiply it by b
 function multiply(a, b) {
     let result = 0;
+    let sign = 1;
+    if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
+        sign *= -1
+        if (a < 0) {
+            a *= -1
+        }
+        if (b < 0) {
+            b *= -1
+        }
+    }
     while (b > 0) {
         result += a;
         b--;
     }
-    return result;
+    return result*sign;
 };
 
 // Iteratively subtract b from a to divide a by b
@@ -26,6 +36,6 @@ function modulo(a, b) {
     return a;
 }
 
-console.log(multiply(2, 6))
+console.log(multiply(123, -22))
 console.log(divide(7, 3))
 console.log(modulo(7, 3))
