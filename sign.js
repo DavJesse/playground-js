6b2098ff19c37b4b34a19edfbe53706cd5b9304b
davodhiambo <jesseomolo@yahoo.com>
2024-08-29 05:39:59 +0300

    feat: add shorthand, hybrid and longhand versions of sign functions to sign.js.

---
+++ sign.js
@@ -0,0 +1,30 @@
+// Sign return 1 for positive numbers, -1 for negative, and 0 for zero
+// Shorthand
+//const sign = (num) => num > 0 ? 1 : num < 0 ? -1 : 0;
+
+// Hybrid
+// const sign = (num) => {
+//     if (num > 0) {
+//         return 1;
+//     } else if (num < 0) {
+//         return -1;
+//     } else {
+//         return 0;
+//     }
+// }
+
+// Longhand
+function sign(num) {
+    if (num > 0) {
+        return 1
+    } else if (num < 0) {
+        return -1
+    } else {
+        return 0
+    }
+}
+
+// Print to console
+// console.log(sign(5))
+// console.log(sign(-12))
+// console.log(sign(0))
