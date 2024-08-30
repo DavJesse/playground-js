fabe2a81f41dd8e1c283460f6d1555c2f0488e1d
davodhiambo <jesseomolo@yahoo.com>
2024-08-30 16:13:35 +0300

    feat: add indexOf function to index-of.js.

---
+++ index-of.js
@@ -0,0 +1,29 @@
+function indexOf(arr, el, startIndex=0) {
+    let i = startIndex
+
+    if (i < 0) {
+        i += arr.length;
+        if (i < 0) {
+            i = 0;
+        };
+    };
+
+    while (i < arr.length) {
+        if (arr[i] === el) {
+            return i;
+        };
+
+        if (i === arr.length -1) {
+            return -1;
+        };
+        i++;
+    };
+};
+
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+console.log(indexOf(beasts, 'bison'))
+console.log(indexOf(beasts, 'giraffe'))
+console.log(indexOf(beasts, 'bison', 2))
+console.log(indexOf(beasts, 'camel', -2))
+console.log(indexOf(beasts, 'camel', -100))
