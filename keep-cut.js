805a5e7a532049669733ebb045cc64df1a142cea
davodhiambo <jesseomolo@yahoo.com>
2024-08-30 23:18:54 +0300

    files: add keep-cut.js.

---
+++ keep-cut.js
@@ -0,0 +1,13 @@
+const cutFirst = (str) => str.slice(2);
+const cutLast = (str) => str.slice(0, str.length-2);
+const cutFirstLast = (str) => cutLast(cutFirst(str));
+const keepFirst = (str) => str.slice(0, 2);
+const keepLast = (str) => str.slice(str.length-2);
+const keepFirstLast = (str) => keepFirst(str) + keepLast(str);
+
+// console.log(cutFirst("Hello World"))
+// console.log(cutLast("Hello World"))
+// console.log(cutFirstLast("Hello World"))
+// console.log(keepFirst("Hello World"))
+// console.log(keepLast("Hello World"))
+// console.log(keepFirstLast("Hello World"))
