18012f4a5391624ebeca28f73421907d05dd8304
davodhiambo <jesseomolo@yahoo.com>
2024-08-30 16:21:05 +0300

    feat: add lastIndexOf function to index-of.js.

--- index-of.js
+++ index-of.js
@@ -20,6 +20,28 @@ function indexOf(arr, el, startIndex=0) {
     };
 };
 
+function lastIndexOf(arr, el, startIndex=arr.length) {
+    let i = startIndex
+
+    if (i < 0) {
+        i += arr.length;
+        if (i < 0) {
+            i = arr.length;
+        };
+    };
+
+    while (i >= 0) {
+        if (arr[i] === el) {
+            return i;
+        };
+
+        if (i === arr.length -1) {
+            return -1;
+        };
+        i--;
+    };
+};
+
 const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
 
 console.log(indexOf(beasts, 'bison'))
