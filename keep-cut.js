6447aedd33c48e408befadb523ed9f7adaeae0e3
davodhiambo <jesseomolo@yahoo.com>
2024-08-30 23:29:10 +0300

    fix: adjust keepFirstLast function keep-cut.js.

--- keep-cut.js
+++ keep-cut.js
@@ -3,11 +3,19 @@ const cutLast = (str) => str.slice(0, str.length-2);
 const cutFirstLast = (str) => cutLast(cutFirst(str));
 const keepFirst = (str) => str.slice(0, 2);
 const keepLast = (str) => str.slice(str.length-2);
-const keepFirstLast = (str) => keepFirst(str) + keepLast(str);
+
+function keepFirstLast(str) {
+    if (str.length > 2) {
+        return keepFirst(str) + keepLast(str)
+    } else {
+        return str
+    }
+}
+//const keepFirstLast = (str) => keepFirst(str) + keepLast(str);
 
 // console.log(cutFirst("Hello World"))
 // console.log(cutLast("Hello World"))
 // console.log(cutFirstLast("Hello World"))
 // console.log(keepFirst("Hello World"))
 // console.log(keepLast("Hello World"))
-// console.log(keepFirstLast("Hello World"))
+ console.log(keepFirstLast('af'))
