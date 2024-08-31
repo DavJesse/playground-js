4b2769efb68688a228c2794500d2e24d5699ae83
davodhiambo <jesseomolo@yahoo.com>
2024-08-31 22:04:32 +0300

    feat: add sameAmount to same-amount.js

---
+++ same-amount.js
@@ -0,0 +1,9 @@
+function sameAount(str, regx1, regx2) {
+    if (regx1.test(str) && regx2.test(str)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// console.log(sameAount('Hello world', /^[hH]ello/, /world$/ ))
