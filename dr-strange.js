ac9e799022e1a0464fa132e39a45c50a4eb4153c
davodhiambo <jesseomolo@yahoo.com>
2024-09-02 17:00:04 +0300

    feat: add addWeek and timeTravel functions to dr-strange.js

---
+++ dr-strange.js
@@ -0,0 +1,38 @@
+function addWeek(date) {
+
+    const days = [
+        'Monday', 'Teusday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
+        'secondMonday', 'secondTeusday', 'secondWednesday', 'secondThursday', 'secondFriday', 'secondSaturday', 'secondSunday'
+    ];
+
+    const epoch = new Date('0001-01-01');
+    const diffTime = date.getTime() - epoch.getTime();
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24))
+
+    const dayIndex = diffDays % 14;
+
+    return days[dayIndex];
+}
+
+function timeTravel({ date, hour, minute, second }) {
+    const newDate = new Date(date);
+
+    if (hour !== undefined) newDate.setHours(hour);
+    if (minute !== undefined) newDate.setHours(minute);
+    if (second !== undefined) newDate.setHours(second);
+
+    return newDate
+}
+
+// console.log(addWeek(new Date('0001-01-01')));
+// console.log(addWeek(new Date('0001-01-02')));
+// console.log(addWeek(new Date('0001-01-07')));
+// console.log(addWeek(new Date('0001-01-08')));
+// console.log(addWeek(new Date('0001-01-09')));
+
+console.log(timeTravel({
+    date: new Date('2020-05-29 23:25:22'),
+    hour: 21,
+    minute: 22,
+    second: 22,
+  }).toString());
