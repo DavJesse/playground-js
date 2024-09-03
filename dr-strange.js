b5558ba3c7f55dbd3b1b89d0c18965e9c8ed6bc3
davodhiambo <jesseomolo@yahoo.com>
2024-09-03 08:44:24 +0300

    feat: add addWeek and timeTravel functions to dr-strange.js

--- dr-strange.js
+++ dr-strange.js
@@ -30,9 +30,9 @@ function timeTravel({ date, hour, minute, second }) {
 // console.log(addWeek(new Date('0001-01-08')));
 // console.log(addWeek(new Date('0001-01-09')));
 
-// console.log(timeTravel({
-//     date: new Date('2020-05-29 23:25:22'),
-//     hour: 21,
-//     minute: 22,
-//     second: 22,
-//   }).getTime());
+console.log(timeTravel({
+    date: new Date('2020-05-29 23:25:22'),
+    hour: 21,
+    minute: 22,
+    second: 22,
+  }).getTime());
