7bbd27c10b2fe48006060cd77343599e9a34027d
davodhiambo <jesseomolo@yahoo.com>
2024-09-04 10:55:28 +0300

    files: addsunny-sunday.js

---
+++ sunny-sunday.js
@@ -0,0 +1,12 @@
+function sunnySunday(date) {
+    const regularDay = date.getDay();
+    const sunnyDay = regularDay === 0 ? 5 : regularDay - 1;
+    const weekdays = ['Monday', 'Tuesday',  'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    console.log(sunnyDay)
+    return weekdays[sunnyDay];
+}
+
+// //console.log(sunnySunday(new Date('2024-09-02'))); // Should print "Monday"
+// console.log(sunnySunday(new Date('2024-09-01'))); // Should print "Saturday" because it's a Sunday and Sunday is skipped
+// //console.log(sunnySunday(new Date('2024-09-07'))); // Should print "Friday"
