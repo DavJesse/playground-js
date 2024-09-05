d336ad018c7492121a2889c032b1c6140fcbbbf8
davodhiambo <jesseomolo@yahoo.com>
2024-09-05 18:50:12 +0300

    files: add filter.js.

--- filter.js
+++ filter.js
@@ -2,7 +2,14 @@ function filter(arr, func) {
     const result = [];
     if (Array.isArray) {
         for (let i = 0; i < arr.length; i++) {
-
+            if (func(arr[i], i, arr)) {
+                result.push;
+            }
         }
     }
+    return result;
 }
+
+const numbers = [1, 2, 3, 4, 5];
+const greaterThanThree = filter(numbers, num => num > 3);
+console.log(greaterThanThree);
