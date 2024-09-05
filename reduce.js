cb57081c6f3ee38cd7ad707c38eeb689e28c3a01
davodhiambo <jesseomolo@yahoo.com>
2024-09-05 21:43:22 +0300

    files: add reduce.js

--- reduce.js
+++ reduce.js
@@ -36,8 +36,8 @@ function reduceRight(arr, func) {
     return acc;
 }
 
-const adder = (a, b) => a + b
+//const adder = (a, b) => a + b
 // console.log(fold([1, 2, 3], adder, 2)) // returns 8 (2 + 1 + 2 + 3)
 // console.log(foldRight([1, 2, 3], adder, 2)) // returns 8 (2 + 3 + 2 + 1)
-console.log(reduce([1, 2, 3], adder)) // returns 6 (1 + 2 + 3)
-console.log(reduceRight([1, 2, 3], adder)) // returns 6 (3 + 2 + 1)
+// console.log(reduce([1, 2, 3], adder)) // returns 6 (1 + 2 + 3)
+// console.log(reduceRight([1, 2, 3], adder)) // returns 6 (3 + 2 + 1)
