8ee6cd974a44038d827b61b307a8196999ae986a
davodhiambo <jesseomolo@yahoo.com>
2024-09-05 16:32:23 +0300

    files: add mapper.js.

---
+++ mapper.js
@@ -0,0 +1,35 @@
+function map(arr, func) {
+    const result = [];
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            result.push(func(arr[i], i, arr))
+        }
+    }
+    return result;
+}
+
+function flatMap(arr, func) {
+    const result = []
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            const processed = func(arr[i], i, arr)
+            
+            for (let j = 0; j < processed.length; j++) {
+                result.push(processed[j])
+            }
+        }
+    }
+    return result
+}
+
+// const users = [
+//     { name: 'Alice', age: 25 },
+//     { name: 'Bob', age: 30 },
+//     { name: 'Charlie', age: 35 }
+//   ];
+// const names = map(users, user => user.name);
+// console.log(names);
+
+const sentences = ['Hello world', 'How are you'];
+const words = flatMap(sentences, sentence => sentence.split(' '));
+console.log(words);
