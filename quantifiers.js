39b28982b7fca5a3b031a65989bd2f6833ce2b5c
davodhiambo <jesseomolo@yahoo.com>
2024-09-05 22:01:48 +0300

    files: add qualifiers.js.

---
+++ quantifiers.js
@@ -0,0 +1,32 @@
+function every(arr, func) {
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            if (!func(arr[i])) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function some(arr, func) {
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            if (func(arr[i])) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function none(arr, func) {
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            if (func(arr[i])) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
