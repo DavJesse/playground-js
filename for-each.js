b7144dbfab3bc8d61d04ed956b4e3f25dbd24a64
davodhiambo <jesseomolo@yahoo.com>
2024-09-05 15:23:52 +0300

    files: add for-each.js.

--- for-each.js
+++ for-each.js
@@ -1,5 +1,10 @@
 function forEach(arr, func) {
    for (let i = 0; i < arr.length; i++) {
-    func(arr[i], index, arr);
+    func(arr[i]);
    } 
 }
+
+// const result = []
+// const returned = forEach(arr, value => result.push(value))
+
+// console.log(result)
