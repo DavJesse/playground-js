65e715be8dce7a2ccc0c9dc3059f0a98d337b4ac
davodhiambo <jesseomolo@yahoo.com>
2024-09-05 21:42:35 +0300

    files: add reduce.js

--- reduce.js
+++ reduce.js
@@ -17,7 +17,7 @@ function foldRight(arr, func, acc) {
 }
 
 function reduce(arr, func) {
-    let acc;
+    let acc = typeof arr[0] === 'string' ? '' : 0;
     if (Array.isArray(arr)) {
         for (let i = 0; i < arr.length; i++) {
             acc = func(acc, arr[i]);
@@ -27,7 +27,7 @@ function reduce(arr, func) {
 }
 
 function reduceRight(arr, func) {
-    let acc;
+    let acc = typeof arr[0] === 'string' ? '' : 0;
     if (Array.isArray(arr)) {
         for (let i = arr.length-1; i >= 0; i--) {
             acc = func(acc, arr[i]);
@@ -36,8 +36,8 @@ function reduceRight(arr, func) {
     return acc;
 }
 
-// const adder = (a, b) => a + b
+const adder = (a, b) => a + b
 // console.log(fold([1, 2, 3], adder, 2)) // returns 8 (2 + 1 + 2 + 3)
 // console.log(foldRight([1, 2, 3], adder, 2)) // returns 8 (2 + 3 + 2 + 1)
-// console.log(reduce([1, 2, 3], adder)) // returns 6 (1 + 2 + 3)
-// console.log(reduceRight([1, 2, 3], adder)) // returns 6 (3 + 2 + 1)
+console.log(reduce([1, 2, 3], adder)) // returns 6 (1 + 2 + 3)
+console.log(reduceRight([1, 2, 3], adder)) // returns 6 (3 + 2 + 1)
