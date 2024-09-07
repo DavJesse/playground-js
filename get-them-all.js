0ba78e3345f764a115954ea246cfd0957920dd1b
davodhiambo <jesseomolo@yahoo.com>
2024-09-07 17:29:29 +0300

    files: add get-them-all files.

--- get-them-all.js
+++ get-them-all.js
@@ -1,23 +1,23 @@
 export const getArchitects = () => {
-  const architects = Array.from(document.getElementsByTagName('a'));
-  const nonArchitects = Array.from(document.getElementsByTagName('span'));
-  return [architects, nonArchitects];
-};
+    const architects = Array.from(document.getElementsByTagName('a'));
+    const nonArchitects = Array.from(document.getElementsByTagName('span'));
+    return [architects, nonArchitects];
+}
 
 export const getClassical = () => {
     const classical = Array.from(document.getElementsByClassName('classical'));
-    const nonClassical = Array.from(document.querySelectorAll('.modern', '.baroque'));
+    const nonClassical = Array.from(document.querySelectorAll('.modern, .baroque'));
     return [classical, nonClassical];
-};
+}
 
 export const getActive = () => {
-    const activeClassical = Array.from(document.getElementsByClassName('active'));
-    const nonActiveClassical = Array.from(document.querySelectorAll('a:not(.active), span:not(.active)'));
-    return [activeClassical, nonActiveClassical];
-};
+    const active = Array.from(document.getElementsByClassName('active'));
+    const nonActive = Array.from(document.querySelectorAll('a:not(.active), span:not(.active)'));
+    return [active, nonActive];
+}
 
 export const getBonannoPisano = () => {
-    const Bonanno = document.getElementById('BonannoPisano');
-    const restActiveClassical = Array.from(document.querySelectorAll('a.classical.active:not(#BonannoPisano)'));
-    return [Bonanno, restActiveClassical]
-};
+    const bonannoPisano = document.getElementById('BonannoPisano');
+    const otherActiveClassical = Array.from(document.querySelectorAll('a.classical.active:not(#BonannoPisano)'));
+    return [bonannoPisano, otherActiveClassical];
+}
