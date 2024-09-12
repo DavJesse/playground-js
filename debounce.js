735bee6758855e4981c5782aaef667c92f1a64dd
davodhiambo <jesseomolo@yahoo.com>
2024-09-12 20:53:59 +0300

    feat: add opDebounce.

--- debounce.js
+++ debounce.js
@@ -2,12 +2,25 @@ function debounce(func, delay) {
     let timeOut;
     return function(...args) {
         clearTimeout(timeOut);
-        timeOut = setTimeout(() => func(...args), delay)
+        timeOut = setTimeout(() => func.apply(this, args), delay)
     };
 }
 
-function opDebounce() {
+function opDebounce(func, delay, option = {}) {
+    let timeOut;
 
+    return function(...args) {
+        function later() {
+            timeOut = null;
+            if (!option.leading) func.apply(this, args);
+        };
+
+        clearTimeout(timeOut);
+        timeOut = setTimeout(later, delay)
+
+        if (option.leading && !timeOut) func.apply(this, args)
+
+    };
 }
 
-opDebounce()
+//opDebounce()
