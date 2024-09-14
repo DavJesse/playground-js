d7198823fc4aae7497dc62cbc9775f37e96addbe
davodhiambo <jesseomolo@yahoo.com>
2024-09-14 19:52:23 +0300

    files: add is-winner.js.

--- is-winner.js
+++ is-winner.js
@@ -1,18 +1,18 @@
 async function isWinner(country) {
     try {
         country = await db.getWinner(country);
-        if (country.continet !== 'Europe') {
-            return `${country} is not what we are looking for because of continent`;
-        }
         if (country === Error('Country not Found')) {
             return `${country.name} never was a winner`;
         }
-        let result = await db.getResults(country.id);
-        if (result.length < 3) {
-            return `${country.name} is not what we are looking for because of number of times it was a champion`;
+        if (country.continet !== 'Europe') {
+            return `${country} is not what we are looking for because of continent`;
         }
+        let result = await db.getResults(country.id);
         if (result === Error('Results found')) {
         return `${country.name} never was a winner`;
+        }
+        if (result.length < 3) {
+            return `${country.name} is not what we are looking for because of number of times it was champion`;
         }
         return (
             `${country.name} won the FIFA World Cup in ` + result.map((result) => result.year).join(', ') +
