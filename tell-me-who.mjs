8a680c5e4c3bd13deefbeb5002826f8707f5321c
davodhiambo <jesseomolo@yahoo.com>
2024-09-17 16:23:09 +0300

    files: add tell-me-who.mjs

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -5,12 +5,14 @@ const path = argv[2];
 
 fs.readdir(path, (err, files) => {
     if (!err) {
+        let count = 1;
         files.sort((a, b) => {
             a - b
         });
 
         for (const file of files) {
-            console.log(file);
+            console.log(count + '. ' + file);
+            count++;
         }
         process.exit(0)
     } else {
